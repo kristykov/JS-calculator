f47ea1428150d0c58ea347c4bdc0f10f2c0a1947
Krystsina Kavalevich <krikoax@gmail.com>
2022-12-02 13:45:52 +0100

    feat: add theme switcher functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./style.scss";
+
+/*= =========  Light theme ========== */
+
+const calculatorEl = document.querySelector(".calculator");
+const themeSwitcher = document.querySelector(".switch");
+
+themeSwitcher.addEventListener("click", () => {
+  if (themeSwitcher.checked === true) {
+    calculatorEl.classList.add("light-theme");
+  } else {
+    calculatorEl.classList.remove("light-theme");
+  }
+});
